80bed03e163c19a24dfa5250237f2780c2b12974
“Mrityunjay <mrityunjay38@gmail.com>
2023-01-23 00:03:05 +0530

    Fake traffic chart plot

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "antd/dist/antd.css";
+import "@ant-design/flowchart/dist/index.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
