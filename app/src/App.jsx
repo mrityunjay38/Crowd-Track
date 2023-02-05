6a8117cb64e59a0706667f244f0bca19371dba0c
“Mrityunjay <mrityunjay38@gmail.com>
2023-02-05 17:27:57 +0530

    Chart scale setup

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,7 +9,8 @@ import { Line, Bar } from "@ant-design/plots";
 import { Empty, Row, Col } from "antd";
 const MAX_CONNECTION = 100;
 const MAX_DURATION = 60 * 1;
-const MAX_PIPELINE = 2;
+const MAX_PIPELINE = 1;
+const TIME_FRAME = 5;
 let MAX_Y_SCALE = MAX_CONNECTION;
 
 const baseConfig = {
@@ -25,31 +26,44 @@ const baseConfig = {
       duration: 100,
     },
   },
-  slider: {
-    start: 0,
-    end: 1,
-  },
 };
 
 const lineChartConfig = {
   ...baseConfig,
   xField: "x",
   yField: "y",
+  slider: {
+    start: 0,
+    end: 1,
+  },
 };
 
 const barChartConfig = {
   ...baseConfig,
+  xField: "y",
+  yField: "x",
+  scrollbar: {
+    type: "vertical",
+  },
 };
 
 function App() {
   const [trafficCount, setTrafficCount] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [chartData, setChartData] = useState({});
 
   useEffect(() => {
     const socket = io(BASE_SOCKET);
     userEmitter(socket);
     subscribeListner(socket);
+
+    /* Pre-define line chart scale */
+    setChartData(defineAxisScale());
+
     return () => unsubscribeListner(socket);
   }, []);
+
+  useEffect(() => {}, [timer]);
 
   const userEmitter = (socket) => {
     socket.emit("new_user");
@@ -63,7 +77,32 @@ function App() {
     socket.disconnect();
   };
 
+  const startTimer = () => {
+    const hook = setInterval(() => {
+      setTimer((prev) => prev + 1);
+    }, 5000);
+    return hook;
+  };
+
+  const endTimer = (hook) => clearInterval(hook);
+
+  const defineAxisScale = () => {
+    const points = new Array(MAX_DURATION / TIME_FRAME)
+      .fill(TIME_FRAME)
+      .reduce((acc, point, index) => {
+        if (index !== 0) acc.push(acc[index - 1] + point);
+        else acc.push(point);
+        return acc;
+      }, [])
+      .reduce((acc, point) => {
+        acc[point] = 0;
+        return acc;
+      }, {});
+    return points;
+  };
+
   const simulateTraffic = () => {
+    const hook = startTimer();
     axios
       .get(SIMULATOR_API + "/simulate-traffic", {
         params: {
@@ -73,8 +112,14 @@ function App() {
           targetUrl: "http://127.0.0.1:3001",
         },
       })
-      .catch((err) => err);
+      .catch((err) => err)
+      .then(() => endTimer(hook));
   };
+
+  const plotChartData = Object.entries(chartData).map(([key, val]) => ({
+    x: key,
+    y: val,
+  }));
 
   return (
     <Row className="container">
@@ -84,10 +129,10 @@ function App() {
       </Col>
       <Col span={18} className="chart-plot flex col justify center">
         <div id="line">
-          <Line {...lineChartConfig} data={[]} />
+          <Line {...lineChartConfig} data={plotChartData} />
         </div>
         <div id="bar">
-          <Bar {...barChartConfig} data={[]} />
+          <Bar {...barChartConfig} data={plotChartData} />
         </div>
       </Col>
     </Row>
